d987e2dfafa5d8491182d5e0396ce918a7c1d28f
RyanLilleyman <lilleymanryan2020@gmail.com>
2023-08-15 06:46:32 -0400

    Add .gitignore

---
+++ i_o/target/debug/deps/i_o-8758323399dcd6e1.d
@@ -0,0 +1,5 @@
+/home/ryan/Repos/smallIO/i_o/target/debug/deps/i_o-8758323399dcd6e1.rmeta: src/main.rs
+
+/home/ryan/Repos/smallIO/i_o/target/debug/deps/i_o-8758323399dcd6e1.d: src/main.rs
+
+src/main.rs:
---
+++ i_o/target/debug/deps/i_o-bfb3f08097e2d0af.d
@@ -0,0 +1,5 @@
+/home/ryan/Repos/smallIO/i_o/target/debug/deps/i_o-bfb3f08097e2d0af.rmeta: src/main.rs
+
+/home/ryan/Repos/smallIO/i_o/target/debug/deps/i_o-bfb3f08097e2d0af.d: src/main.rs
+
+src/main.rs:
